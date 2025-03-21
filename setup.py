de132d30c0a740f27e02f4c29e681bb9b58fe23e
Zachary Jones <zjones.2012@icloud.com>
2025-03-21 10:57:19 -0500

    update to 1.0.0: remove tests and fix requirements.txt

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="incus_sdk",
-    version="0.1.0",
+    version="1.0.0",
     author="Incus SDK Team",
-    author_email="info@example.com",
+    author_email="zacharyj@orbical.dev",
     description="A Python SDK for the Incus API",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Tests for the Incus Python SDK.
-"""
